fe5750f1c8662603bb317dba2ee3f542c9c039d7
Branden Phan <branden.phan@gmail.com>
2021-06-19 23:08:36 -0400

    Update Signup.js

--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -43,9 +43,9 @@ const Signup = () => {
 	return (
 		<>
 			<BackgroundContainer>
-				<Grid container spacing={2} style={{ width: "100%" }}>
+				<Grid container spacing={0} style={{ width: "100%" }}>
 					<Grid item xs={12}>
-						<Grid container justify="center" spacing={2}>
+						<Grid container justify="center" spacing={0}>
 							<FormContainer>
 								<br />
 								<br />
